c7768b782e2bc1f5ee79ea226d8c776f490745fe
Wiffdle <118768543+wiffdle@users.noreply.github.com>
2023-11-06 15:07:03 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,16 @@ let siteWidth = 1280;
 let scale = screen.width / siteWidth;
 document.querySelector('meta[name="viewport"]').setAttribute('content', 'width='+siteWidth+', initial-scale='+scale+'');
 
+if (getCookie("darkmode")) {
+  Background = document.querySelector("body");
+  Background.classList.add("Dark");
+  Background.classList.remove("Light");
+  let stateObj = { id: "100" };
+  window.history.pushState(stateObj,
+    "Page", "/?dark");
+    setCookie('darkmode', 'true', '70');
+}
+
 const playerList = [
   "Alec Warda",
   "Ryan Kracht",
@@ -108,6 +118,7 @@ function DarkMode()
     let stateObj = { id: "100" };
     window.history.pushState(stateObj,
       "Page", "/?light");
+      eraseCookie('darkmode');
   }
   else {
     Background.classList.add("Dark");
@@ -115,7 +126,31 @@ function DarkMode()
     let stateObj = { id: "100" };
     window.history.pushState(stateObj,
       "Page", "/?dark");
+      setCookie('darkmode', 'true', '70');
   }
+}
+
+function setCookie(name,value,days) {
+  var expires = "";
+  if (days) {
+      var date = new Date();
+      date.setTime(date.getTime() + (days*24*60*60*1000));
+      expires = "; expires=" + date.toUTCString();
+  }
+  document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+}
+function getCookie(name) {
+  var nameEQ = name + "=";
+  var ca = document.cookie.split(';');
+  for(var i=0;i < ca.length;i++) {
+      var c = ca[i];
+      while (c.charAt(0)==' ') c = c.substring(1,c.length);
+      if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+  }
+  return null;
+}
+function eraseCookie(name) {
+  document.cookie = name +'=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
 }
 
 function createLabel(){
