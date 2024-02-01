782a9e24692df78afec6eff5fb2c4240e3f7d360
MichaelDors <115369454+MichaelDors@users.noreply.github.com>
2024-02-01 09:28:26 -0500

    Update app.js

--- app.js
+++ app.js
@@ -221,9 +221,11 @@ function initDaily(){
   var diff = (now - start) + ((start.getTimezoneOffset() - now.getTimezoneOffset()) * 60 * 1000);
   var oneDay = 1000 * 60 * 60 * 24;
   var day = Math.floor(diff / oneDay);
-  if(getCookie("dailydone") == day){
-    alert("you've already completed today's challenge");
-    location.reload();
+  if(getCookie("dailydone")){
+    if(getCookie("dailydone") == day){
+      alert("you've already completed today's challenge");
+      location.reload();
+    } 
   }
   getDailyPlayer();
   setCookie('dailydone', day, '70');
